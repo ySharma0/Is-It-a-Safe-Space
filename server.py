f026d9ea115a1cb50cdbe495b20a18695f925834
Yash Sharma <mr.sharmayash@outlook.com>
2021-03-14 07:56:53 -0700

    checked text against model

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 import os
 from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import scoped_session, sessionmaker
+from test import model_call
 os.system('pip install torch==1.8.0+cpu -f https://download.pytorch.org/whl/torch_stable.html')
 
 if not os.getenv("DATABASE_URL"):
@@ -18,32 +19,6 @@ options = webdriver.ChromeOptions()
 options.binary_location = "/app/.apt/usr/bin/google-chrome-stable"
 options.add_argument("--headless")
 driver = webdriver.Chrome(chrome_options=options)
-
-class NLP_model(nn.Module):
-    def __init__(self, num_words, emb_size, num_classes):
-        super().__init__()
-        self.num_words = num_words
-        self.emb_size = emb_size
-        self.emb = nn.Embedding(self.num_words, self.emb_size)
-        self.emb.from_pretrained(vocab.vectors)
-        self.lstm = nn.LSTM(input_size = emb_size, hidden_size = 32, batch_first = True, num_layers = 2)
-        self.relu = nn.ReLU()
-        self.lin = nn.Linear(64, num_classes)
-        
-    def forward(self, batch_data):
-        token_embs = self.emb(batch_data)
-        outputs, (h_n, c_n) = self.lstm(token_embs)
-        
-        last_hidden_state = h_n
-        last_hidden_state = last_hidden_state.permute(1, 0, 2)
-        last_hidden_state = last_hidden_state.flatten(start_dim = 1)
-
-        last_hidden_state = self.relu(last_hidden_state)
-        logits = self.lin(last_hidden_state)
-        
-        return logits
-def preprocess_input(text):
-    print(text)
 
 
 regex = re.compile(
@@ -80,8 +55,22 @@ def getData():
         
         #push url to db
         # 0 : hate speech    1 : profainity   2 : neither
+        prof_count = 0
+        hate_count = 0
+        for i in soup:
+            class_type = model_call(str(i))
+            if class_type == 0:
+                hate_count+=1
+            if class_type == 1:
+                prof_count +=1
         
-        classification = "profanity"
+        if (prof_count+hate_count)/len(soup) < 0.3:
+            classification = "safe"
+        else:
+            if prof_count > hate_count: 
+                classification = "profanity"
+            else:
+                classification = "hate"
         db.execute("INSERT INTO urls VALUES(:url, :type, :freq)",{"url":url, "type": classification, "freq": 1})
         db.commit()      
 
