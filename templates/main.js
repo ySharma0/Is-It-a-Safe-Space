9fbba3ed166c2be3c1983746c7a35bba681ab61a
Yash Sharma <mr.sharmayash@outlook.com>
2021-03-13 18:55:12 -0800

    testing

--- templates/main.js
+++ templates/main.js
@@ -3,7 +3,12 @@ const axios = require('axios');
 function checkURL(){
     axios.get("https://is-it-a-safe-space-data.herokuapp.com/getData?url=" + url).then(resp => {
 
-        console.log(resp.data);
+        resp = resp.data
+        if(resp = "success"){
+            alert("Your website is a safe space")
+        }else{
+            
+        }
     
     });    
 }
