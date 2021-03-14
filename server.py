ab83d203dc94e48af48c766dfd4b006ecf5e170a
Yash Sharma <mr.sharmayash@outlook.com>
2021-03-13 22:25:19 -0800

    adding layout

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, Response, render_template, redirect
-from flask_cors import CORS, cross_origin
 from bs4 import BeautifulSoup
 import re
 from selenium import webdriver
 import os
-# from sqlalchemy import create_engine, exc
-# from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine, exc
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-setup selenium chrome
-options = webdriver.ChromeOptions()
-options.binary_location = "/app/.apt/usr/bin/google-chrome-stable"
-options.add_argument("--headless")
-driver = webdriver.Chrome(chrome_options=options)
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATBASE_URL is not set")
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+# setup selenium chrome
+# options = webdriver.ChromeOptions()
+# options.binary_location = "/app/.apt/usr/bin/google-chrome-stable"
+# options.add_argument("--headless")
+# driver = webdriver.Chrome(chrome_options=options)
 
 regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
@@ -31,13 +36,24 @@ def getData():
     url = str(request.values.get("url"))
     
     if re.match(regex, url) is not None:
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source.encode("utf-8"), "lxml") # grab text
-        #clean text
-        soup = os.linesep.join([s for s in soup.text.splitlines() if s])
-        soup = soup.split("\n")
+        x = db.execute("select type, freq from urls where url='"+url+"';").fetchone()
+        if x:
+            
+        # driver.get(url)
+        # soup = BeautifulSoup(driver.page_source.encode("utf-8"), "lxml") # grab text
+        # #clean text
+        # soup = os.linesep.join([s for s in soup.text.splitlines() if s])
+        # soup = soup.split("\n")
         
-        return str(soup)
+        # #push url to db
+        # classification = "safe"
+        # db.execute("INSERT INTO urls VALUES(:url, :type, :freq)",{"url":url, "type": classification, "freq": 1})
+        # db.commit()
+        
+
+        return str("Badly Formatted URL")
     else:
         return url
+         
+
 
